c1fb7722fac8cd2107c33aac31426c8d674a976e
Marcus20119 <hoanglongnguyenhuu20119@gmail.com>
2022-08-11 17:49:54 +0700

    upload

---
+++ .history/main_20220809194220.js
@@ -0,0 +1,39 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+function addBodyScroll() {
+   $('body').classList.add('modal-open');
+}
+function removeBodyScroll() {
+   $('body').classList.remove('modal-open');
+}
+
+$('.trailer__arrow--next').onclick = function() {
+   $('.trailer__carousel-main-next').click();
+   $('.trailer__carousel-sub-next').click();
+};
+$('.trailer__arrow--prev').onclick = function() {
+   $('.trailer__carousel-main-prev').click();
+   $('.trailer__carousel-sub-prev').click();
+};
+
+for (let i = 1; i <= 3; i++) {
+   $(`#trailer-${i}__close`).onclick = function() {
+      const html = $(`#trailer-${i}__video`).innerHTML;
+      $(`#trailer-${i}__video`).innerHTML = '';
+      removeBodyScroll();
+      setTimeout(function() {
+         $(`#trailer-${i}__video`).innerHTML = html;
+      }, 1000);
+   };
+}
+
+let modalShows = $$('.modal__show-js');
+Array.from(modalShows).forEach(function(modalShow) {
+   modalShow.onclick = addBodyScroll;
+});
+
+let modalHides = $$('.modal__hide-js');
+Array.from(modalHides).forEach(function(modalHide) {
+   modalHide.onclick = removeBodyScroll;
+});
---
+++ .history/main_20220810182707.js
@@ -0,0 +1,84 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+function addBodyScroll() {
+   $('body').classList.add('modal-open');
+}
+function removeBodyScroll() {
+   $('body').classList.remove('modal-open');
+}
+
+$('.trailer__arrow--next').onclick = function() {
+   $('.trailer__carousel-main-next').click();
+   $('.trailer__carousel-sub-next').click();
+};
+$('.trailer__arrow--prev').onclick = function() {
+   $('.trailer__carousel-main-prev').click();
+   $('.trailer__carousel-sub-prev').click();
+};
+
+for (let i = 1; i <= 3; i++) {
+   $(`#trailer-${i}__close`).onclick = function() {
+      const html = $(`#trailer-${i}__video`).innerHTML;
+      $(`#trailer-${i}__video`).innerHTML = '';
+      removeBodyScroll();
+      setTimeout(function() {
+         $(`#trailer-${i}__video`).innerHTML = html;
+      }, 1000);
+   };
+}
+
+let modalShows = $$('.modal__show-js');
+Array.from(modalShows).forEach(function(modalShow) {
+   modalShow.onclick = addBodyScroll;
+});
+
+let modalHides = $$('.modal__hide-js');
+Array.from(modalHides).forEach(function(modalHide) {
+   modalHide.onclick = removeBodyScroll;
+});
+
+const characters = [
+   'argyle',
+   'dustin',
+   'eddie',
+   'eleven',
+   'erica',
+   'hopper',
+   'jonathan',
+   'joyce',
+   'lucas',
+   'max',
+   'mike',
+   'murray',
+   'nancy',
+   'robin',
+   'steve',
+   'will'
+];
+
+const pages = Array(16).fill(0);
+function switchPage(currentPage, checkboxs) {
+
+}
+
+characters.forEach(function(character, index) {
+   const modal = $(`#character-${character}__modal`);
+   if (modal) {
+      const nextBtn = modal.querySelector('.character__modal-next');
+      const prevBtn = modal.querySelector('.character__modal-prev');
+      const checkboxs = [...modal.querySelectorAll('.character__modal-checkbox')];
+      const quantity = checkboxs.length;
+
+      console.log(checkboxs);
+      console.log(quantity);
+
+      nextBtn.onclick = () => {
+         pages[index]++;
+         // if(pages[index] >)
+      };
+      prevBtn.onclick = () => {
+         pages[index]--;
+      };
+   }
+});
